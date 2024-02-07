afc28c6e8f1aa022e2cc5e5f52e4ebf46207e498
JS-Develope <jeanaparecido21@gmail.com>
2024-02-06 22:51:02 -0300

    Primeiro commit

---
+++ back-end/os/src/main/java/com/generator/os/GeneratorOsApplication.java
@@ -0,0 +1,13 @@
+package com.generator.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorOsApplication.class, args);
+	}
+
+}
---
+++ back-end/os/src/test/java/com/generator/os/GeneratorOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generator.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
